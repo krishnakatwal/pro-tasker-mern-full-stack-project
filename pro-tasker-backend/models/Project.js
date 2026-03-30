18005f2cfdf4f0a17242a6b98b3cef2fba87c095
krishnakatwal <kousalkc@gmail.com>
2026-03-30 11:53:20 -0400

    add mongoose schema models for user, project, and task

---
+++ pro-tasker-backend/models/Project.js
@@ -0,0 +1,26 @@
+import mongoose from "mongoose";
+
+const projectSchema = new Schema({
+  name: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  description: {
+    type: String,
+    trim: true,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+  user: {
+    type: Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
+  },
+});
+
+const Project = mongoose.model("Project", projectSchema);
+
+export default Project;
---
+++ pro-tasker-backend/models/Task.js
@@ -0,0 +1,30 @@
+import mongoose from "mongoose";
+
+const taskSchema = new Schema({
+  title: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  description: {
+    type: String,
+    trim: true,
+  },
+  status: {
+    type: String,
+    enum: ["To Do", "In Progress", "Done"], // only these values allowed
+    default: "To Do", // default status
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+  project: {
+    type: mongoose.Schema.ObjectId,
+    ref: Project, // reference to the Project model
+    required: true,
+  },
+});
+
+const Task = mongoose.model("Task", taskSchema);
+export default Task;
