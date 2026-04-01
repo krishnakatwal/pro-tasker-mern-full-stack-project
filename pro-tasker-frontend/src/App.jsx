d5c2b297bc7dc72ae360618b1f7b0ae0bdfb1929
krishnakatwal <kousalkc@gmail.com>
2026-03-31 23:11:55 -0400

    add navbar component

--- pro-tasker-frontend/src/App.jsx
+++ pro-tasker-frontend/src/App.jsx
@@ -1,29 +1,34 @@
+import "./App.css";
 import Register from "./pages/Register.jsx";
 import Login from "./pages/Login.jsx";
-import "./App.css";
+
 import { Routes,Route,Navigate } from "react-router-dom";
 import { useUser } from "./context/UserContext.jsx";
+import Navbar from "./components/Navbar.jsx";
 
 function App() {
 
+
   //bring in user info 
   const {user} = useUser()
+
   return (
     <>
+     <h1>Welcome</h1>
       {/* inserts the navigation bar at the top of the page. */}
-      {/* <Navbar /> */}
-      <h1>Welcome</h1>
+      <Navbar />
+     
       {user ? (
         <Routes>
-          <Route></Route>
-          <Route></Route>
+          {/* <Route path='/dashboard' element={<Dashboard/ >} /> */}
+          <Route />
         </Routes>
 
       ):(
         <Routes>
             <Route path="/register" element={<Register/>} />
-            <Route></Route>
-            <Route></Route>
+            <Route path="/login" element={<Login/>} />
+            <Route path="*" element={<Navigate to="/login"/>} />
               
         </Routes>
       )}
---
+++ pro-tasker-frontend/src/components/Navbar.jsx
@@ -0,0 +1,37 @@
+import { Link } from "react-router-dom";
+import { useUser } from "../context/UserContext.jsx";
+
+
+function Navbar() {
+  //bring in user info from context
+  const { user, logout } = useUser();
+
+  return (
+    <nav>
+      {user && <p>WelCome{user.username}</p>}
+      <ul>
+        {user ? (
+          <>
+            <li>
+              <Link to="/dashboard">Dashboard</Link>
+            </li>
+            <li onClick={logout}>
+              <Link to="/login">Logout</Link>
+            </li>
+          </>
+        ) : (
+          <>
+            <li>
+              <Link to="/login">Login</Link>
+            </li>
+            <li>
+              <Link to="/register">Register</Link>
+            </li>
+          </>
+        )}
+      </ul>
+    </nav>
+  );
+}
+
+export default Navbar;
